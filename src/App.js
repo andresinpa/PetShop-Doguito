b005c03def76d8ef5a8ae1f16cafa961f86ed876
andresinpa <andresinfantepaez@gmail.com>
2023-06-04 17:38:44 -0500

    Diseño y funcionalidad completa. Fin del curso React Router: Navegación en una SPA.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Page404 from './pages/page404';
+import Header from './components/header';
+import Post from './pages/Post';
+import Categoria from './pages/Categoria';
 
 function App() {
-  return (
-    <>
 
-    </>
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/posts/:id' element={<Post/>}/>
+        <Route path='/categoria/:id/*' element={<Categoria/>}/>
+        <Route path='*' element={<Page404/>}/>
+        
+      </Routes>
+    </Router>
   );
 }
 
